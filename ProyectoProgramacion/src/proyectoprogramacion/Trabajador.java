824c75c054f7b91027755c870e4eb6011ad0257b
Angelik <angeguateca31@gmail.com>
2024-11-03 15:53:04 -0600

    Update Trabajador.java

--- ProyectoProgramacion/src/proyectoprogramacion/Trabajador.java
+++ ProyectoProgramacion/src/proyectoprogramacion/Trabajador.java
@@ -9,5 +9,53 @@ package proyectoprogramacion;
  * @author AMD
  */
 public class Trabajador {
-    
+    //declaración de variables
+    public int edad=0;
+    public String nom="";
+    public String puesto="";
+    private String ID="";
+    //partes del barista
+    public static enum menu{
+        NormalCoffe, Capuchino, CapuchinoV, Chocolate, Moka, Te_chai, iceCoffe
+    }
+
+    public Usuario(int edad, String nom, String puesto, String ID) {
+        this.edad=edad;
+        this.nom=nom;
+        this.puesto=puesto;
+        this.ID=ID;
+          
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    } 
 }
