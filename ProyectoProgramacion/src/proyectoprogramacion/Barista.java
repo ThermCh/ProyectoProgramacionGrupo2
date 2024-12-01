5ae79e7dddb746082b789bd45d567d3e00ad3776
Angelik <angeguateca31@gmail.com>
2024-12-01 12:22:31 -0600

    Update Barista.java
    Eliminación del switch, aunque aun me falta que sirva el getHour

--- ProyectoProgramacion/src/proyectoprogramacion/Barista.java
+++ ProyectoProgramacion/src/proyectoprogramacion/Barista.java
@@ -14,48 +14,39 @@ public class Barista {
 
     public String hour = "";
     public static String bebida = ";
-    public static int condi = 0;
 
-   public static void drinkAssignment(int menu, String bebida, int condi) {
-        if (condi != 1) {
-            switch (menu) {
-                case 1:
-                    bebida = "Café normal";
-                    condi = condi + 1;
-                    break;
-                case 2:
-                    bebida = "Capuchino";
-                    condi = condi + 1;
-                    break;
-                case 3:
-                    bebida = "Capuchino Vainilla";
-                    condi = condi + 1;
-                    break;
-                case 4:
-                    bebida = "Chocolate";
-                    condi = condi + 1;
-                    break;
-                case 5:
-                    bebida = "Moka";
-                    condi = condi + 1;
-                    break;
-                case 6:
-                    bebida = "Te chai";
-                    condi = condi + 1;
-                    break;
-                case 7:
-                    bebida = "Café frió";
-                    condi = condi + 1;
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "la opción que ingreso no es válida");
-
-            }
-            JOptionPane.showMessageDialog(null, "su bebida es: " + bebida);
-        } else {
-            JOptionPane.showMessageDialog(null, "Ya ingreso su bebida anteriormente. Es: " + bebida);
-        }
+    public Barista() {
     }
 
+    public static void Hora(String hour) {
+        Random aleatorio = new Random();
+        int hora = aleatorio.nextInt(6, 17);
+        int min = aleatorio.nextInt(0, 59);
+        hour = hora + ":" + min;
+        
 
+    }
+   
+
+    public String getHour() {
+        Barista.Hora(hour);
+        
+        return hour ;
+    }
+
+    public void setHour(String hour) {
+         Barista.Hora(hour);
+       
+        this.hour = hour;
+    }
+
+    public String getBebida() {
+        return bebida;
+    }
+
+    public void setBebida(String bebida) {
+        this.bebida = bebida;
+    }
+
+   
 }
